a1f8a8646219fba44b3f734d6d8c1b29426abe92
Akash Tyagi <akashdktyagi@gmail.com>
2022-12-10 21:19:20 +0800

    Added docker compose file for pact flow broker. Added Maven pluign to publish the pact from consumer to broker

--- consumer/src/main/java/com/automationfraternity/EmployeeController.java
+++ consumer/src/main/java/com/automationfraternity/EmployeeController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class EmployeeController {
