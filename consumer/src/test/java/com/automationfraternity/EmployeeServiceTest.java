0a43cca9f9e8fac25b8c2bab9d5cea1caed83e98
Akash Tyagi <akashdktyagi@gmail.com>
2022-12-10 17:14:25 +0800

    Consumer pact test refactoring changes

--- consumer/src/test/java/com/automationfraternity/EmployeeServiceTest.java
+++ consumer/src/test/java/com/automationfraternity/EmployeeServiceTest.java
@@ -29,18 +29,23 @@ class EmployeeServiceTest {
     }
 
     @Test
-    public void test_get_list_of_all_employees(WireMockRuntimeInfo wireMockRuntimeInfo){
+    public void test_get_list_of_all_employees(WireMockRuntimeInfo wireMockRuntimeInfo) throws JsonProcessingException {
+        Employee employee = Employee.builder().withId(12).withEmpId(1).withName("Akash").withAge(56).withEmail("a@a.com").withDepartment("HR").withDesignation("recruiter").withPhone("2324235").withSalary("15000").build();
+        Employee employee1 = Employee.builder().withId(13).withEmpId(2).withName("Amit").withAge(56).withEmail("b@b.com").withDepartment("IT").withDesignation("manager").withPhone("2324235").withSalary("10000").build();
+        Employee employee2 = Employee.builder().withId(14).withEmpId(3).withName("Sumit").withAge(56).withEmail("s@s.com").withDepartment("finance").withDesignation("director").withPhone("2324235").withSalary("20000").build();
+
+        List<Employee> employeeList = Arrays.asList(employee,employee1,employee2);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String body = objectMapper.writeValueAsString(employeeList);
+
         WireMock wireMock = wireMockRuntimeInfo.getWireMock();
         wireMock.register(get((urlPathEqualTo("/employee")))
                 .willReturn(aResponse()
                 .withStatus(200)
                 .withHeader("Content-Type", "application/json")
-                .withBody("[{\"id\":12,\"empId\":1,\"name\":\"Akash\",\"age\":56,\"email\":\"a@a.com\",\"phone\":\"2324235\",\"department\":\"HR\",\"salary\":\"15000\",\"designation\":\"recruiter\"},{\"id\":13,\"empId\":2,\"name\":\"Amit\",\"age\":56,\"email\":\"b@b.com\",\"phone\":\"2324235\",\"department\":\"IT\",\"salary\":\"10000\",\"designation\":\"manager\"},{\"id\":14,\"empId\":3,\"name\":\"Sumit\",\"age\":56,\"email\":\"s@s.com\",\"phone\":\"2324235\",\"department\":\"finance\",\"salary\":\"20000\",\"designation\":\"director\"}]")
+                .withBody(body)
         ));
 
-        Employee employee = Employee.builder().withId(12).withEmpId(1).withName("Akash").withAge(56).withEmail("a@a.com").withDepartment("HR").withDesignation("recruiter").withPhone("2324235").withSalary("15000").build();
-        Employee employee1 = Employee.builder().withId(13).withEmpId(2).withName("Amit").withAge(56).withEmail("b@b.com").withDepartment("IT").withDesignation("manager").withPhone("2324235").withSalary("10000").build();
-        Employee employee2 = Employee.builder().withId(14).withEmpId(3).withName("Sumit").withAge(56).withEmail("s@s.com").withDepartment("finance").withDesignation("director").withPhone("2324235").withSalary("20000").build();
         List<Employee> employeeListExpected = Arrays.asList(employee,employee1,employee2);
         List<Employee> employeeListActual = Arrays.asList(employeeService.getListOfEmployees().getBody());
         Assertions.assertThat(employeeListExpected).isEqualTo(employeeListActual);
